7f64a1811d577863e73652c182b5a153914ab24e
Adrian Lafont-Mueller <adrianl0@chicagobooth.edu>
2022-07-12 02:08:24 +0200

    submitting test code

--- problem2.py
+++ problem2.py
@@ -1,23 +1,21 @@
 # Write a PROGRAM to count the number of vowels in a word.
 
 def validate_input(word):
-    """Docstring here"""
-    #
-    # Your code here
-    #
-    return __a_bool__
+    """ensure that the word is valid"""
+    if word.isalpha():
+        return True
 
 def count_vowels(word):
-    """Docstring here"""
-    #
-    # Your code here
-    #
+    """count number of vowels"""
     return __Something__Here__
 
-
+def main():
+    validate_input()
+    count_vowels()
 
 #
 if __name__ == "__main__":
    #
    #
    #
+
